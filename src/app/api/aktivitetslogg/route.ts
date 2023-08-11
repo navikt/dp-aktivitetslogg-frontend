82a08b5799c321626d0dad7678d450b022639281
André Roaldseth <andre.roaldseth@nav.no>
2023-08-11 18:47:41 +0200

    force-dynamic i get rout

--- src/app/api/aktivitetslogg/route.ts
+++ src/app/api/aktivitetslogg/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { getAktivitetsloggOboToken, getAzureSession } from "@/lib/auth";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(request: Request) {
   console.log("route handler", process.env);
   const session = await getAzureSession(request);
