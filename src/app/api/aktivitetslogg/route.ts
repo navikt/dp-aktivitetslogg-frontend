9dd28d6e32bc8f6db60dd25e4ad5f7f5758dfaf3
André Roaldseth <andre.roaldseth@nav.no>
2023-08-13 12:14:16 +0200

    Sett revalidate til 0

--- src/app/api/aktivitetslogg/route.ts
+++ src/app/api/aktivitetslogg/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import { getAktivitetsloggOboToken, getAzureSession } from "@/lib/auth";
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export async function GET(request: Request) {
   console.log("route handler", process.env);
