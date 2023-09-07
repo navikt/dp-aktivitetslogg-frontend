4f4d78f3699a7d3eb96d2d4e55adc67338ab4e25
Frode Sjovatsen <frode.sjovatsen@nav.no>
2023-09-07 16:16:32 +0200

    Vi kan nå hente ut en liste av tjenester

---
+++ src/app/api/aktivitetslogg/tjenester/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { getAktivitetsloggOboToken, getAzureSession } from "@/lib/auth";
+
+export const revalidate = 0;
+
+export async function GET(request: Request) {
+  const session = await getAzureSession(request);
+
+  if (!session)
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+
+  const url = new URL(
+    process.env.API_BASE_URL || "http://dp-aktivitetslogg/aktivitetslogg",
+  );
+  url.pathname = `${url.pathname}/tjenester`;
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${await getAktivitetsloggOboToken(session)}`,
+    },
+  });
+
+  return NextResponse.json(await response.json());
+}
