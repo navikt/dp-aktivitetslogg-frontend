f1bbed61d907b65ad19f39532aa232343a7f1486
Frode Sjovatsen <frode.sjovatsen@nav.no>
2023-09-13 16:44:31 +0200

    Bruker dynamisk import på jscrypt komponent

--- src/app/aktivitetslogg/page.tsx
+++ src/app/aktivitetslogg/page.tsx
@@ -1,6 +1,11 @@
 import type { NextPage } from "next";
 import styles from "@/app/page.module.css";
-import AktivitetsloggContainer from "../../components/aktivitetslogg";
+
+import dynamic from "next/dynamic";
+const AktivitetsloggContainer = dynamic(
+  () => import("../../components/aktivitetslogg"), // path of your component
+  { ssr: false },
+);
 import React from "react";
 import Header from "@/components/header";
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,11 +5,13 @@ import { InternalHeader, Spacer } from "@navikt/ds-react";
 
 function Header() {
   const [fulltNavn, setFulltNavn] = useState<string>("");
+
   useEffect(() => {
     fetch("/api/me")
       .then((value) => value.json())
       .then((value) => setFulltNavn(`${value.givenName} ${value.surname}`));
   }, []);
+
   return (
     <InternalHeader>
       <InternalHeader.Title as="h1">Aktivitetslogg</InternalHeader.Title>
